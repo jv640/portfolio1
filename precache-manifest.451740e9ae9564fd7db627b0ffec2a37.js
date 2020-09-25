26700feb254cf40484e18d4e80a113d33dc9e5a0
jv640 <vermajatin640@gmail.com>
2020-09-25 17:56:19 +0530

    Updates

---
+++ precache-manifest.451740e9ae9564fd7db627b0ffec2a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccf9611f48f61c256c3f38dac98b338",
+    "url": "/portfolio1/index.html"
+  },
+  {
+    "revision": "2a5f97569a5446295c16",
+    "url": "/portfolio1/static/css/main.f0ace881.chunk.css"
+  },
+  {
+    "revision": "068628fdfd85e8c23c41",
+    "url": "/portfolio1/static/js/2.26fa8bee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio1/static/js/2.26fa8bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5f97569a5446295c16",
+    "url": "/portfolio1/static/js/main.76bc4336.chunk.js"
+  },
+  {
+    "revision": "c98ff24afaa0a7d926fb",
+    "url": "/portfolio1/static/js/runtime-main.616a7c63.js"
+  }
+]);
