c757c998609abdb60ae9d9bfe68b622de98058df
jv640 <vermajatin640@gmail.com>
2020-09-29 11:10:53 +0530

    coorected about me

--- src/constants/About.js
+++ src/constants/About.js
@@ -1,5 +1,5 @@
 export const about = {
     heading: "About Me",
     quote: "Code is like humor. When you have to explain it, it’s bad.",
-    bio: "A learner by nature and developer by passion, a tech enthusiast, a full-stack developer, poet, competititve programmer.\n\nJatin has a vision to bring good impactful changes in life of each and every individual of this world\n\n And some random shit here"
+    bio: "A learner by nature and developer by passion, a tech enthusiast, a full-stack developer, poet, competititve programmer.\n\nJatin has a vision to bring good impactful changes in life of each and every individual of this world\n\n"
 };
